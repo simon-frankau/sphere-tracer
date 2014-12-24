83f82fc553c73b37834b3572ba90c37c172a2c1a
Simon Frankau <sgf@arbitrary.name>
2014-12-24 15:31:03 +0000

    Added 'trans.c', which will be used for a transparency/refraction demo

--- build.sh
+++ build.sh
@@ -5,3 +5,4 @@ gcc dof.c tracer.c png_render.c -lpng -O2 -Wall -std=c99 -o dof
 gcc soft.c tracer.c png_render.c -lpng -O2 -Wall -std=c99 -o soft
 gcc fuzzy.c tracer.c png_render.c -lpng -O2 -Wall -std=c99 -o fuzzy
 gcc moblur.c tracer.c png_render.c -lpng -O2 -Wall -std=c99 -o moblur
+gcc trans.c tracer.c png_render.c -lpng -O2 -Wall -std=c99 -o trans
