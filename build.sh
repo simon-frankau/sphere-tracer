4746ef0e5a6ee4ef5a7ce9f6d87ba4fca2fd8ddf
Simon Frankau <sgf@arbitrary.name>
2015-01-02 01:22:59 +0000

    Added 'dof2', which is 'dof' with soft shadows

--- build.sh
+++ build.sh
@@ -6,3 +6,4 @@ gcc soft.c tracer.c png_render.c -lpng -O2 -Wall -std=c99 -o soft
 gcc fuzzy.c tracer.c png_render.c -lpng -O2 -Wall -std=c99 -o fuzzy
 gcc moblur.c tracer.c png_render.c -lpng -O2 -Wall -std=c99 -o moblur
 gcc trans.c tracer.c png_render.c -lpng -O2 -Wall -std=c99 -o trans
+gcc dof2.c tracer.c png_render.c -lpng -O2 -Wall -std=c99 -o dof2
